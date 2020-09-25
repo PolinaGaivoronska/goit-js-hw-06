26e6e714b83a62447255d7cf7116f325802fa1d9
Polina Gaivoronska <gaivoronska.art@gmail.com>
2020-09-25 23:50:43 +0300

    Добавляет файлы пятой и шестой задач

---
+++ js/task-05.js
@@ -0,0 +1,3 @@
+const getUserWithEmail = (array, mail) => array.find(({email}) => email === mail);
+
+console.log(getUserWithEmail(users, 'rossvazquez@xinware.com'));
---
+++ js/task-06.js
@@ -0,0 +1,5 @@
+const getUsersWithAge = (array, min, max) => array
+ .filter(({age}) => age > min && age < max)   
+ .map(({name, email}) => ({name, email}));  
+
+console.log(getUsersWithAge(users, 20, 30));
